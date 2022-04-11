c7f4423cd923276a488cc77ccc10463cbd0e7c0b
Sebastian Paul <sebastian.paul01@sap.com>
2022-04-11 14:34:26 +0200

    update 4

--- src/main/java/hwr/oop/examples/HelloWorld.java
+++ src/main/java/hwr/oop/examples/HelloWorld.java
@@ -1,12 +1,7 @@
 package hwr.oop.examples;
 
-<<<<<<< Updated upstream
 class HelloWorld {
     String getOutputString() {
-=======
-public class HelloWorld {
-    public String getOutputString(){
->>>>>>> Stashed changes
         return "Hello World";
     }
 }
